3a3f98f491a233d1153b58676e2e056659414bbd
Heorhii Siburov <g.siburov@gmail.com>
2019-09-06 08:28:28 -0700

    switched to class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Women's World Cup</h1>
 
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+ 
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Women's World Cup</h1>
+  
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
