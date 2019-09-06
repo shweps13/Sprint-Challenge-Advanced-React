97bd35b01074114a01f4670404e835a726f73e33
Heorhii Siburov <g.siburov@gmail.com>
2019-09-06 08:44:53 -0700

    data given to child

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import './App.css';
 
+import DisplayCard from "./components/DisplayCard"
 
-class App extends Component {
-  constructor(){
-    super();
- 
+class App extends React.Component {
+
+    constructor(){
+      super();
+      this.state = {
+        players : []
+    };
   }
   
 
   componentDidMount() {
     fetch("http://localhost:5000/api/players")
       .then(res => res.json())
-      .then(res => console.log(res))
-      // .then(res => this.setState({cards : res}))
+      .then(res => this.setState({players : res}))
+      .then(console.log("hook", this.state.players))
       .catch(err => console.log("Something goes wrong with main request"));
 
   }
@@ -24,8 +28,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>Women's World Cup</h1>
-  
         </header>
+        <DisplayCard players={this.state.players} />
       </div>
     );
   }
---
+++ client/src/components/DisplayCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function DisplayCard(props) {
+console.log("Data received to child: ", props)
+
+  return (
+    <div className="Usercard">
+        <p>Ololo</p>
+          
+    </div>
+  );
+}
+
+export default DisplayCard;
